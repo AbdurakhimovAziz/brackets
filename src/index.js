737f84df084ae16530d466d81db6c14bbcf3a4f5
Aziz <68072877+AbdurakhimovAziz@users.noreply.github.com>
2021-02-23 23:03:09 +0500

    feat: finish brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
+  const parsedBracketsConfig = bracketsConfig.map(el => `${el[0]}${el[1]}`);
+  let strLength = str.length;
+
+  while (str !== '') {
+    parsedBracketsConfig.forEach(r => (str = str.replace(r, '')));
+    if (strLength === str.length) {
+      return false;
+    }
+    strLength = str.length;
+  }
+  return true;
   // your solution
-}
+};
